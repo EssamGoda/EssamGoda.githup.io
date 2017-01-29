40611b5f12c7f8f30b7736f1e941cef322483a8a
Essam Goda <engessamgoda@gmail.com>
2017-01-29 21:36:22 +0200

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,16 +1,11 @@
-﻿<?php
-
+<?php
 // Replace this with your own email address
 $siteOwnersEmail = 'engessamgoda@gmail.com';
-
-
 if($_POST) {
-
    $name = trim(stripslashes($_POST['contactName']));
    $email = trim(stripslashes($_POST['contactEmail']));
    $subject = trim(stripslashes($_POST['contactSubject']));
    $contact_message = trim(stripslashes($_POST['contactMessage']));
-
    // Check Name
 	if (strlen($name) < 2) {
 		$error['name'] = "Please enter your name.";
@@ -25,62 +20,38 @@ if($_POST) {
 	}
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
-
-
    // Set Message
    $message .= "Email from: " . $name . "<br />";
 	$message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
-
    // Set From: header
    $from =  $name . " <" . $email . ">";
-
    // Email Headers
 	$headers = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $email . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-
    if (!$error) {
 $PHPMailer = new PHPMailer();
       
 $PHPMailer->CharSet="UTF-8";
-
-$PHPMailer->IsHTML(true);
-
-      
-$PHPMailer->IsSMTP(True);
-$PHPMailer->Host = 'localhost';//سيرفر الـ SMTP 
-$PHPMailer->isHtml(True);//True or false
-$PHPMailer->From = $email;//بريد المرسل
-
-$PHPMailer->FromName = $name;//اسم المرسل
-
-  $PHPMailer->AddAddress($siteOwnersEmail);//بريد المستقبل
-
-  $PHPMailer->Subject  = $subject;//عنوان الرسالة
-
-  $PHPMailer->Body = $message;//نص الرسالة  
-$PHPMailer->Send();
-
+$PHPMailer->setFrom($email,$name);//ÈÑíÏ ÇáãÑÓá
+  $PHPMailer->addAddress($siteOwnersEmail);//ÈÑíÏ ÇáãÓÊÞÈá
+  $PHPMailer->Subject  = $subject;//ÚäæÇä ÇáÑÓÇáÉ
+  $PHPMailer->altBody = $message;//äÕ ÇáÑÓÇáÉ  
+$PHPMailer->send();
 		if ($PHPMailer->send()) { echo "OK"; }
       else { echo "Something went wrong. Please try again."; }
 		
 	} # end if - no validation error
-
 	else {
-
 		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
 		
 		echo $response;
-
 	} # end if - there was a validation error
-
 }
-
 ?>
